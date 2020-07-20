b9e0d41bc907b19a6e09c90d128607f1c393c6bf
mindcandyy <68534319+mindcandyy@users.noreply.github.com>
2020-07-20 14:28:37 +0700

    Create Exercise4-Yada-Klueabvichit.py

---
+++ Exercise4-Yada-Klueabvichit.py
@@ -0,0 +1,14 @@
+Eng = 90.0
+GenBus = 80.0
+IntroCom = 85.0
+ComProg = 82.0
+
+print("--- Your Score ---")
+
+print("Foundation English : " + Eng)
+
+print("General Business : "+ GenBus)
+
+print("Introduction to Computer Systems : "+ IntroCom)
+
+print("Computer Programming : " + ComProg)
